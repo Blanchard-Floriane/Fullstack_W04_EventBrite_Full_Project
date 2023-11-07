958e9c07e7f14627e66b4332925f7a06e20e455a
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-07 12:21:06 +0100

    add nav bar and routes to event index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   get "up" => "rails/health#show", as: :rails_health_check
   
   root to: 'events#index'
