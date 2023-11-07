f2428f6ccb6be57cfbe099ce13e5ce5dbb5900fd
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-07 16:39:28 +0100

    current user set up in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def new
