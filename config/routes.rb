c85b3b446103583be5c738c5d4ec391d52beb670
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-07 10:02:26 +0100

    Attendance migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+  
+  root to: 'events#index'
+  resources :attendances
   resources :events
   devise_for :users
-  
-  get "up" => "rails/health#show", as: :rails_health_check
-
-
 end
