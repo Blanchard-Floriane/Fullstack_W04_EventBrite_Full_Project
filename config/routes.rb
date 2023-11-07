7c4344bba8c7832356a645e219a4e8ea69757b90
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-07 10:46:18 +0100

    Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get "up" => "rails/health#show", as: :rails_health_check
   
   root to: 'events#index'
