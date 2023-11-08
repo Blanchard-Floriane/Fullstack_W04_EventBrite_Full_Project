83ac5e1d97b3eacf3f2d42bc3470c73519dcd787
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-08 15:17:50 +0100

    migration Add Admin infos to Event table

---
+++ db/migrate/20231108135547_remove_column_is_admin_to_user.rb
@@ -0,0 +1,5 @@
+class RemoveColumnIsAdminToUser < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :users, :is_admin, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_07_085904) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_135853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_07_085904) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "administrator_id"
+    t.index ["administrator_id"], name: "index_events_on_administrator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,11 +46,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_07_085904) do
     t.text "description"
     t.string "first_name"
     t.string "last_name"
-    t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "administrator_id"
 end
