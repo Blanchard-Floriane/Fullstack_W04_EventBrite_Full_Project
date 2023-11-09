29a36a9c53c0aff4d822fde0714aaa37bca9fbe3
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-09 11:51:44 +0100

    stripe version 2019 done and successfully working in local, now test with heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-  #root 'orders#new'
-  resources :orders, only: [:new, :create]
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
 end
