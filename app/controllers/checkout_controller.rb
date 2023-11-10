5d2d96f40c3b1b9debf9a4f6b92d8c1273c0ccaf
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-10 08:57:43 +0100

    stripe button and checkout controller set up so now if a user participate to an event, this create a new attendance line

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,10 @@
 class CheckoutController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :success, :cancel]
+
   def create
     @total = params[:total].to_d
+    @event_id = params[:event_id]
+
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -9,13 +13,18 @@ class CheckoutController < ApplicationController
             currency: 'eur',
             unit_amount: (@total*100).to_i,
             product_data: {
-              name: 'Rails Stripe Checkout',
+              name: "Participation à : " + Event.find(@event_id).title
             },
           },
           quantity: 1
         },
       ],
+      #to store arbitary but non sensible data
+      metadata: {
+        event_id: @event_id
+      },
       mode: 'payment',
+      customer_email: current_user.email,
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
@@ -23,8 +32,16 @@ class CheckoutController < ApplicationController
   end
 
   def success
+    @user = current_user
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    @event_id = @session.metadata.event_id
+
+    Attendance.create(
+      event_id: @event_id,
+      user_id: @user.id,
+      stripe_customer_id: @session.id
+    )
   end
 
   def cancel
